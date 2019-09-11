9d05e19ee77ced56607844da838e80e27f1a307a
David Griswold <david.griswold@graded.br>
2019-09-11 01:30:05 +0000

    edited settings for tasks and added comments

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
 // DO NOT TOUCH THE ABOVE CODE IF YOU WANT THE MAGIC!
 // (You can remove or add an extra slash to the top @ts-check line if you want things to be a bit less strict)
 
+/** This function runs as soon as the p5 library loads */
 function setup() {
   createCanvas(400, 600);
+  // set up some settings to make everything centered by default
   textAlign(CENTER, CENTER);
   rectMode(CENTER);
 }
 
+/** This function runs in an infinitely repeating loop */
 function draw() {
   if (mouseIsPressed) {
     fill("black");
@@ -23,7 +26,7 @@ function draw() {
 
   writeText();
 }
-
+/* This function draws our welcome text and its rectangle background */
 function writeText() {
   fill("pink");
   stroke("black");
